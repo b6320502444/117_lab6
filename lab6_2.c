e15d66c80bfe45ac2c12b50a980d467f7c4797f2
b6320502444 <preechaya.chi@ku.th>
2021-03-15 19:05:10 +0700

    .

---
+++ lab6_2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,m=0;
+    scanf("%d",&n);
+    char a[2][n];
+    for(i=0;i<2;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        if(a[0][i] == a[1][i])
+        m++;
+    }
+    printf("%d %d",m,n-m);
+return 0;
+}
+
